f6b86f8dbf4a4c5eed37b71d6fd0f168d15cdefa
Jonathan East <jonathan_east@ymail.com>
2020-02-26 20:35:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function theBeatlesPlay(musicians, instruments) {
   var array = [];
 
   for (var i = 0; i < musicians.length; i++) {
-    array.push(`${musicians} plays the ${instruments}`);
+    array.push(`${musicians[i]} plays the ${instruments[i]}`);
   }
   return array;
 }
